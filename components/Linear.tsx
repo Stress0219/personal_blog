abff1af2543b6b169669448536fb9836e30ae97e
Stres0219 <joan.comas12@gmail.com>
2024-09-17 03:55:06 -0400

    linear_adjusted

--- components/Linear.tsx
+++ components/Linear.tsx
@@ -1,29 +1,29 @@
-'use client'
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import LinearProgress from '@mui/material/LinearProgress';
+"use client";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import LinearProgress from "@mui/material/LinearProgress";
 
 export default function LinearDeterminate() {
   const [progress, setProgress] = React.useState(0);
 
   React.useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((oldProgress) => {
-        if (oldProgress === 100) {
-          return 0;
-        }
-        const diff = Math.random() * 10;
-        return Math.min(oldProgress + diff, 100);
-      });
-    }, 500);
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const docHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      const scrollProgress = (scrollTop / docHeight) * 100;
+      setProgress(scrollProgress);
+    };
+
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      clearInterval(timer);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box sx={{ width: "100%", position: "fixed", top: 0, zIndex: 100 }}>
       <LinearProgress variant="determinate" value={progress} />
     </Box>
   );
