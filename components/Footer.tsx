feb7a1e480a2a2f1bfdbe44eb228b85bcab5673a
Stres0219 <joan.comas12@gmail.com>
2024-09-17 00:54:41 -0400

    change_text_size

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-blue-900 h-56 w-full'>
-        <p className='text-8xl'>El pieGRANDE</p>
+        <p className='text-6xl'>El pieGRANDE</p>
     </div>
   )
 }
