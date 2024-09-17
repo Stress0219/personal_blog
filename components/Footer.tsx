5bd728160b8d9627e16915f253b38d6fc15728c1
Stres0219 <joan.comas12@gmail.com>
2024-09-17 04:18:43 -0400

    footer_text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-blue-900 h-56 w-full">
-      <p className="text-xl">
-        © 2024 Joan_Dev Blog. Todos los derechos reservados. Diseñado
+    <div className="bg-blue-900 h-32 w-full">
+      <p className="text-2xl text-center text-wrap p-4">
+        © 2024 Joan_Dev Blog.<br/> Todos los derechos reservados. Diseñado
         con pasión y dedicación. Conéctate conmigo en Twitter
         para más novedades y actualizaciones.
       </p>
